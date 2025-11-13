71d15b093543a35f2bd6b0c5d5f471c8dea6f17b
Aleksandar Milicevic <hi@milicevic.dev>
2025-11-13 11:53:11 +0100

    chore: install antfu eslint config and run lint script
    re #3

--- packages/storefront-ui/eslint.config.js
+++ packages/storefront-ui/eslint.config.js
@@ -1,3 +1,4 @@
-import withNuxt from './.playground/.nuxt/eslint.config.mjs'
+import base from "../../eslint.config.mjs";
+import withNuxt from "./.playground/.nuxt/eslint.config.mjs";
 
-export default withNuxt()
+export default withNuxt(base);
