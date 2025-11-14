d4af71d276b068b468db723fdce978e1ab80e70f
Aleksandar Milicevic <hi@milicevic.dev>
2025-11-14 14:15:53 +0100

    feat(storefront-ui): async button component
    fix #6

--- packages/storefront-ui/app.vue
+++ packages/storefront-ui/app.vue
@@ -1,8 +1,24 @@
+<script lang="ts" setup>
+function action() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, 3000);
+  });
+}
+</script>
+
 <template>
-  <ui-prompt-dialog
-    title="Confirm"
-    question="Are you sure?"
-    label-cancel="No"
-    label-confirm="Yes"
-  />
+  <div class="flex flex-col gap-4">
+    <ui-prompt-dialog
+      title="Confirm"
+      question="Are you sure?"
+      label-cancel="No"
+      label-confirm="Yes"
+    />
+
+    <ui-async-button :promise="action">
+      Click Me
+    </ui-async-button>
+  </div>
 </template>
